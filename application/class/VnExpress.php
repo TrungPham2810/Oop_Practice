c2a1b55f2aa85898f0da9e6f3ee9841124e45b1b
TrungPham <nhokpipuppy@gmail.com>
2019-01-08 10:04:13 +0700

    edit

--- application/class/VnExpress.php
+++ application/class/VnExpress.php
@@ -3,7 +3,7 @@ include ('BaseCrawler.php');
 class VnExpress extends BaseCrawler
 {
     private $__search1 = 'class="title_news_detail mb10"';
-    private $__search2 = '</article>';
+    private $__search2 = 'style="text-align:right;"';
     private $__search3 = '>';
     private $__search4 = '<article class="content_detail fck_detail width_common block_ads_connect">';
     
@@ -41,15 +41,29 @@ class VnExpress extends BaseCrawler
 
     public function takeContent()
     {   
-        $g = explode($this->__search4,$this->__deleteGarbage());
-        // bỏ phần chú thíc giữa nội dung và tiêu đề
-        unset($g[0]);
-        $gString = implode(' ',$g);
-        return strip_tags($gString);
-        // $g = $this->__deleteGarbage();
-        // $f = parent::deleteBefore($this->__search3, $this->__deleteGarbage());
-        // $g = str_replace( $f ,' ', $this->__deleteGarbage());
-        // return strip_tags($g);
+        // $g = explode($this->__search4,$this->__deleteGarbage());
+        // // bỏ phần chú thíc giữa nội dung và tiêu đề
+        // unset($g[0]);
+        // $gString = implode(' ',$g);
+        // return strip_tags($gString);
+        // // $g = $this->__deleteGarbage();
+        // // $f = parent::deleteBefore($this->__search3, $this->__deleteGarbage());
+        // // $g = str_replace( $f ,' ', $this->__deleteGarbage());
+        // // return strip_tags($g);
+        $g = str_replace($this->takeTitle(), ' ', $this->__deleteGarbage());
+        // var_dump(htmlentities($g));
+        $g = explode('>',$g,2);
+        // print_r($y[1]);
+        // var_dump($y);
+        // // // var_dump($g[0]);
+        // $z = $y[0].'>';
+        // var_dump($z);
+        // $count = 1;
+        // $g = str_replace($z,' ', $g);
+        // // echo $g;
+        // echo $g[1];
+        // // // $g = implode('',$g);
+        return strip_tags($g[1]);
     }
 }
 ?>
